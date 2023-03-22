6d5ce910c6d88e987cc3f70f887053e7e28de87a
wazabanda <1wazabanda@gmail.com>
2023-03-22 20:13:52 +0200

    my final for today

--- Grade.java
+++ Grade.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class Grade {
-    
+    private Student student;
+    private String course;
+    private ArrayList<Double> quizzes, tests, assignments, projects;
+    private double finalExam;
+
+    public Grade(Student student, String course, ArrayList<Double> quizzes,ArrayList<Double> tests,
+    ArrayList<Double> assignments,ArrayList<Double> projects,double finalExam)
+    {
+        this.student = student;
+        this.course = course;
+        this.quizzes = quizzes;
+        this.tests = tests;
+        this.assignments = assignments;
+        this.projects =projects;
+        this.finalExam = finalExam;
+    }
 }
